fdec2487e62a70236a08aca1e719cab5382f78c0
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-20 16:52:46 -0700

    wrote film model test

--- test/unit/Film.test.js
+++ test/unit/Film.test.js
@@ -4,8 +4,28 @@ const { getErrors } = require('./helpers');
 
 describe('Film model', () => {
 
-    it('is a valid model', () => {
+    it('valid good model', () => {
 
+        const data = {
+            title: 'The Incredibles',
+            studio: 1234546,
+            released: 2004,
+            cast: []
+        };
+
+        const film = new Film(data);
+        data._id = film._id;
+        assert.deepEqual(film.toJSON(), data);
+
+        assert.isUndefinded(film.validateSync());
     });
+
+    it('required fields', () => {
+        const film = new Film({});
+        const errors = getErrors(film.validateSync(), 3);
+        assert.equal(errors.title.kind, 'required');
+        assert.equal(errors.studio.kind, 'required');
+        assert.equal(errors.released.kind, 'required');
+    })
 
 });
