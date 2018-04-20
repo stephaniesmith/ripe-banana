955a73569c615a222ecb956f16b4755ab469343c
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-19 17:38:43 -0700

    updated server with mongodb

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const connect = require('./lib/util/connect');
 
 const PORT = process.env.PORT || 3000;
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongdb://localhost:27017/movies';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/movies';
 
 connect(MONGODB_URI);
 
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-    console.log('server is runing on port', server.address().port);
+    console.log('server is running on port', server.address().port);
 });
