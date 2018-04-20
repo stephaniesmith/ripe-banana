4388f8193d592589e2314604cc197ddde270a620
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-20 16:33:41 -0700

    setting up test valid Film model

---
+++ test/unit/Film.test.js
@@ -0,0 +1,11 @@
+const { assert } = require('chai');
+const Film = require('../../lib/models/Film');
+const { getErrors } = require('./helpers');
+
+describe('Film model', () => {
+
+    it('is a valid model', () => {
+
+    });
+
+});
