885be4ca9aa6c85a22c1ebcaae1378125999f8d1
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-22 13:12:02 -0700

    getting db set up

--- test/e2e/films.test.js
+++ test/e2e/films.test.js
@@ -1,6 +1,7 @@
 const { assert } = require('chai');
 const request = require('./request');
 const { dropCollection } = require('./db');
+const Film = require('../../lib/models/Film');
 
 describe('films API', () => {
 
@@ -10,11 +11,37 @@ describe('films API', () => {
         pob: 'London, UK'
     };
 
+    let sense = {
+        title: 'Sense and Sensibility',
+        studio: 1234546,
+        released: 1995,
+        cast: []
+    };
+
+    let elinor = { 
+        part: 'Elinor Dashwood',
+        actor: emma._id 
+    };
+
+    before(() => dropCollection('actors'));
+    before(() => dropCollection('films'));
+
     before(() => {
         return request.post('/actors')
             .send(emma)
             .then(({ body }) => {
                 emma = body;
+            });
+    });
+
+    it.only('saves a film', () => {
+        console.log('S&S!!', sense);
+        return new Film(sense).save()
+            .then(saved => {
+                saved = saved.toJSON();
+                const { _id } = saved;
+                // console.log(saved);
+                assert.ok(_id);
             });
     });
 
