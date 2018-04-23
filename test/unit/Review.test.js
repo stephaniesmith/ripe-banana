87d2c5e241b51abc567f179ac2bda6516df9b77f
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-23 00:25:37 -0700

    review has min and max validation

--- test/unit/Review.test.js
+++ test/unit/Review.test.js
@@ -29,4 +29,27 @@ describe('Review model', () => {
         assert.strictEqual(errors.review.kind, 'required');
         assert.strictEqual(errors.film.kind, 'required');
     });
+
+    it('includes validation for a minimum value', () => {
+        const review = new Review({
+            rating: 0,
+            reviewer: Types.ObjectId(),            
+            review: 'test',
+            film: Types.ObjectId(),            
+        });
+        const errors = getErrors(review.validateSync(), 1);
+        assert.strictEqual(errors.rating.kind, 'min');
+    });
+
+    it('includes validation for maximum values', () => {
+        const review = new Review({
+            rating: 10,
+            reviewer: Types.ObjectId(),            
+            review: ' Think about it a little and, my god, it\'s about you. Whoever you are. Here is how life is supposed to work. We come out of ourselves and unfold into the world. We try to realize our desires. We fold back into ourselves, and then we die.',
+            film: Types.ObjectId(),            
+        });
+        const errors = getErrors(review.validateSync(), 2);
+        assert.strictEqual(errors.rating.kind, 'max');
+        assert.strictEqual(errors.review.kind, 'maxlength');
+    });
 });
