4be0b9b42233af34128329cb180f2866919d38f8
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-23 09:57:19 -0700

    fix studio routes/tests

--- test/e2e/studios.test.js
+++ test/e2e/studios.test.js
@@ -1,8 +1,6 @@
 const { assert } = require('chai');
 const request = require('./request');
 const { dropCollection } = require('./db');
-//const Studio = require('../../lib/models/Studio');
-
 
 describe('Studio API', () => {
 
@@ -25,55 +23,42 @@ describe('Studio API', () => {
     };
 
     before(() => dropCollection('studios'));
-
-    before(() => {
-        return request.post('/studios')
-            .send(paramount)
-            .then(({ body }) => {
-                paramount = body;
-            });
-    });
     
     it('saves a studio', () => {
         return request.post('/studios')
             .send(paramount)
             .then(({ body }) => {
-                const { _id, _v
-                } = body;
-                paramount[0]._id = body[0]._id;
+                const { _id, __v } = body;
                 assert.ok(_id);
-                assert.equal(_v, 0);
+                assert.equal(__v, 0);
                 assert.deepEqual(body, {
                     ...paramount,
                     _id,
-                    _v
+                    __v
                 });
                 paramount = body; 
             });
     });
 
-    const getFields = ({ _id, name }) => {
-        return {
-            _id, name
-        };
-    };
-         
+    const getFields = ({ _id, name }) => ({ _id, name });         
 
     it('gets all studios', () => {
-        return request.get('/studios') /*Studio.create(paramount).then(roundTrip)
-            .then(saved => {
-        paramount = saved;
-        return request.get('/studios/');
-    })*/
-        .then(({ body }) => {
-            assert.Equal(body, [paramount, pixar].map(getFields));
-        });
+        return request.post('/studios')
+            .send(pixar)
+            .then(({ body }) => {
+                pixar = body;
+                return request.get('/studios');
+            })
+            .then(({ body }) => {
+                assert.deepEqual(body, [paramount, pixar].map(getFields));
+            });
+    });
 
-    it('gets particular studio by id', () => { return request.get(`/studios/${name._id}`)
-        .then(({ body }) => {
-            assert.deepEqual(body, paramount);
-        });
-
+    it('gets particular studio by id', () => { 
+        return request.get(`/studios/${paramount._id}`)
+            .then(({ body }) => {
+                assert.deepEqual(body, paramount);
+            });
     });
 
     it('deletes studio by id', () => {
@@ -83,7 +68,6 @@ describe('Studio API', () => {
             })
             .then(res => {
                 assert.equal(res.status, 404);
-            })
-        }
-    )};
+            });
+    });
 });
