68c45dbf24c4ebf97e26da7891674ed8e41bd25d
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-22 23:43:29 -0700

    Review model passes basic test

---
+++ test/unit/Review.test.js
@@ -0,0 +1,23 @@
+const { assert } = require('chai');
+const { Types } = require('mongoose');
+const Review = require('../../lib/models/Review');
+const { getErrors } = require('./helpers');
+
+describe('Review model', () => {
+    it('is a good, valid model', () => {
+        const info = {
+            rating: 5,
+            reviewer: Types.ObjectId(),
+            review: 'Think about it a little and, my god, it\'s about you. Whoever you are.',
+            film: Types.ObjectId(),
+            createdAt: new Date(),
+            updatedAt: new Date()
+        };
+
+        const review = new Review(info);
+
+        info._id = review._id;
+        assert.deepEqual(review.toJSON(), info);
+        assert.isUndefined(review.validateSync());
+    });
+});
