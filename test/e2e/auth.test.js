949ce684567a0b0e9112e3ec73456d51ff96623b
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-25 17:19:14 -0700

    invalid username and/or password

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -3,7 +3,7 @@ const request = require('./request');
 const { dropCollection } = require('./db');
 
 describe.only('Auth API', () => {
-    beforeEach(() => dropCollection('reviewers'));
+    before(() => dropCollection('reviewers'));
 
     let token = null;
 
@@ -32,6 +32,47 @@ describe.only('Auth API', () => {
             })
             .then(({ body }) => {
                 assert.ok(body.token);
+            });
+    });
+
+    it('Gives 400 on signup of same email', () => {
+        return request
+            .post('/auth/signup')
+            .send({
+                name: 'Roger Ebert',
+                company: 'rogerebert.com',
+                email: 'rober@ebert.com',
+                password: 'abc'
+            })
+            .then(res => {
+                assert.equal(res.status, 400);
+                assert.equal(res.body.error, 'Email exists');
+            });
+    });
+
+    it('Gives 401 on non-existent email', () => {
+        return request
+            .post('/auth/signin')
+            .send({
+                email: 'bad@sue.com',
+                password: 'abc'
+            })
+            .then(res => {
+                assert.equal(res.status, 401);
+                assert.equal(res.body.error, 'Invalid email or password');
+            });
+    });
+
+    it('Gives 401 on bad password', () => {
+        return request
+            .post('/auth/signin')
+            .send({
+                email: 'rober@ebert.com',
+                password: 'bad'
+            })
+            .then(res => {
+                assert.equal(res.status, 401);
+                assert.equal(res.body.error, 'Invalid email or password');
             });
     });
 });
