a4b669b257661926b8e435755c8b846f8952e49d
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-26 09:49:14 -0700

    added createToken helper function

--- test/e2e/db.js
+++ test/e2e/db.js
@@ -1,5 +1,6 @@
 const connect = require('../../lib/util/connect');
 const mongoose = require('mongoose');
+const request = require('./request');
 
 before(() => connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/movies-test'));
 after(() => mongoose.connection.close());
@@ -10,5 +11,16 @@ module.exports = {
             .catch(err => {
                 if(err.codeName !== 'NamespaceNotFound') throw err;
             });
+    },
+    createToken(data = {
+        name: 'Roger Ebert',
+        company: 'rogerebert.com',
+        email: 'rober@ebert.com',
+        password: 'abc'
+    }) {
+        return request
+            .post('/api/auth/signup')
+            .send(data)
+            .then(res => res.body.token);
     }
 };
